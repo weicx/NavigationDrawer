83c7db4884a23ade82a9e2584519ceccc65f2fa5
易小鸣 <weicx1331@163.com>
2015-05-02 23:47:03 +0800

    Initial Commit

---
+++ 抽屉导航/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  抽屉导航
+//
+//  Created by WR on 15-5-2.
+//  Copyright (c) 2015年 WR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 抽屉导航/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  抽屉导航
+//
+//  Created by WR on 15-5-2.
+//  Copyright (c) 2015年 WR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
